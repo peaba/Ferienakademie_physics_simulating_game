e56f8dc32e3303d451a0dff896eb5d2f62bcd3e4
Olivia Odenthal <oo010@hdm-stuttgart.de>
2023-09-19 11:45:24 +0200

    gradient background

--- src/systems/render_systems.cpp
+++ src/systems/render_systems.cpp
@@ -9,31 +9,11 @@ const int screenWidth = 800;
 const int screenHeight = 450;
 
 void toggle_fullscreen_system(flecs::iter) {
-    if (IsKeyPressed(KEY_F11)) {
-        int display = GetCurrentMonitor();
-        if (IsWindowFullscreen()) {
-            // if we are full screen, then go back to the windowed size
-            SetWindowSize(screenWidth, screenHeight);
-        } else {
-            // if we are not full screen, set the window size to match the
-            // monitor we are on
-            SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
-        }
-
-        // toggle the state
-        ToggleFullscreen();
-    }
+    
 }
 
 void update_window(flecs::iter) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
 
-    DrawLine(-100, -100, 100, 100, GREEN);
-
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-    EndDrawing();
 }
 
 RenderSystems::RenderSystems(flecs::world &world) {
@@ -44,13 +24,61 @@ RenderSystems::RenderSystems(flecs::world &world) {
     world.system().kind(flecs::OnUpdate).iter(update_window);
 }
 
+
+Texture2D gradientTex;
+void regenerateGradientTexture(int screenW, int screenH) {
+    UnloadTexture(gradientTex); //TODO necessary?
+    Image verticalGradient = GenImageGradientV(screenW, screenH, BLUE, WHITE);
+    gradientTex = LoadTextureFromImage(verticalGradient);
+    UnloadImage(verticalGradient);
+}
+
 void main_loop(flecs::world &world) {
     InitWindow(screenWidth, screenHeight, WINDOW_NAME);
+    
+    regenerateGradientTexture(screenWidth, screenHeight);
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
         world.progress(dt);
+
+
+
+        if (IsKeyPressed(KEY_F11)) {
+            int display = GetCurrentMonitor();
+            if (IsWindowFullscreen()) {
+                // if we are full screen, then go back to the windowed size
+                SetWindowSize(screenWidth, screenHeight);
+                regenerateGradientTexture(screenWidth, screenHeight);
+
+            } else {
+                // if we are not full screen, set the window size to match the
+                // monitor we are on
+                SetWindowSize(GetMonitorWidth(display),
+                              GetMonitorHeight(display));
+
+                regenerateGradientTexture(GetMonitorWidth(display),
+                                          GetMonitorHeight(display));
+            }
+
+            // toggle the state
+            ToggleFullscreen();
+        }
+    
+        BeginDrawing();
+            ClearBackground(BLUE);
+
+            DrawTexture(gradientTex, 0, 0, WHITE);
+            DrawLine(-100, -100, 100, 100, GREEN);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20,
+                     LIGHTGRAY);
+        EndDrawing();
+    
     }
     CloseWindow();
+
+    UnloadTexture(gradientTex);
 }
