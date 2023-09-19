5ede270aac71f0cf12b21e95dc5ed6d0cd777318
Kilian Northoff <kilian@northoff-online.de>
2023-09-19 11:13:28 +0200

    physic scaffold

---
+++ src/components/particle_state.h
@@ -0,0 +1,12 @@
+#ifndef SURVIVING_SARNTAL_ROCK_COMPONENTS_H
+#define SURVIVING_SARNTAL_ROCK_COMPONENTS_H
+
+struct Position {
+    float x, y;
+};
+
+struct Velocity {
+    float x, y;
+};
+
+#endif // SURVIVING_SARNTAL_ROCK_COMPONENTS_H
---
+++ src/systems/physics.h
@@ -0,0 +1,10 @@
+#ifndef SURVIVING_SARNTAL_PHYSIC_H
+#define SURVIVING_SARNTAL_PHYSIC_H
+
+#include "flecs.h"
+
+struct PhysicSystems {
+    explicit PhysicSystems(flecs::world &world);
+};
+
+#endif // SURVIVING_SARNTAL_PHYSIC_H
