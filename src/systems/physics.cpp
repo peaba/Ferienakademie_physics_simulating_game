ea91cad545dcda83b97994aae1d391e44925cb7c
Bjoern Aheimer 3594348 <st177191@stud.uni-stuttgart.de>
2023-09-21 11:44:22 +0200

    Own class to capsule util functions and make some private, docu and style.

--- src/systems/physics.cpp
+++ src/systems/physics.cpp
@@ -4,35 +4,51 @@
 #include "flecs.h"
 #include "raylib.h"
 
-constexpr float GRAVITATIONAL_CONSTANT = 1000.8;
-
-void makeRock(const flecs::world &world, Position p, Velocity v, Radius r) {
-    flecs::entity new_rock = world.entity();
-    new_rock.set<Position>({p.x, p.y}).set<Velocity>({v.x, v.y})
-        .set<Radius>({r.value}).add<Rock>();
+void debugRenderRocks(flecs::iter it, Position *positions, Radius *r) {
+    for (auto i : it) {
+        DrawCircle(positions[i].x, positions[i].y, r[i].value, RED);
+    }
 }
 
-void updateVelocity(flecs::iter it, Position *positions, Velocity *velocities) {
+PhysicSystems::PhysicSystems(flecs::world &world) {
+    world.module<PhysicSystems>();
+
+    world.system<Position, Velocity>().with<Rock>().iter(
+        RockTools::updateState);
+
+    world.system<Position, Radius>().with<Rock>().iter(debugRenderRocks);
+
+    Position p{500., 500.};
+    Velocity v{0., 0.};
+    Radius r{20.0};
+    RockTools::makeRock(world, p, v, r);
+}
+
+void RockTools::makeRock(const flecs::world &world, Position p, Velocity v,
+                         Radius r) {
+
+    flecs::entity new_rock = world.entity();
+    new_rock.set<Position>({p.x, p.y})
+        .set<Velocity>({v.x, v.y})
+        .set<Radius>({r.value})
+        .add<Rock>();
+}
+
+void RockTools::updateState(flecs::iter it, Position *positions,
+                            Velocity *velocities) {
+    updateVelocity(it, positions, velocities);
+    updatePosition(it, positions, velocities);
+}
+
+void RockTools::updateVelocity(flecs::iter it, Position *positions,
+                               Velocity *velocities) {
     for (auto i : it) {
-
-        /*
-        auto x_min = p.x - r->r;
-        auto x_max = p.x + r->r;
-        IndexInterval interval;
-        interval = m->getRelevantMountainSection(x_min, x_max);
-
-        for (int i = interval.start_indice; i == interval.end_indice; i++) {
-            auto mountain_node = m->getFixpoint(i);
-
-
-        }
-        */
-
         velocities[i].y += GRAVITATIONAL_CONSTANT * it.delta_time();
     }
 }
 
-void updatePosition(flecs::iter it, Position *positions, Velocity *velocities) {
+void RockTools::updatePosition(flecs::iter it, Position *positions,
+                               Velocity *velocities) {
     for (auto i : it) {
         auto p = positions[i];
         auto v = velocities[i];
@@ -42,24 +58,4 @@ void updatePosition(flecs::iter it, Position *positions, Velocity *velocities) {
         positions[i].x += v.x * it.delta_time();
         positions[i].y += v.y * it.delta_time();
     }
-}
-
-void debugRenderRocks(flecs::iter it, Position *positions, Radius *r) {
-    for (auto i: it) {
-        DrawCircle(positions[i].x, positions[i].y, r[i].value, RED);
-    }
-}
-
-PhysicSystems::PhysicSystems(flecs::world &world) {
-    world.module<PhysicSystems>();
-
-    world.system<Position, Velocity>().with<Rock>().iter(updatePosition);
-    world.system<Position, Velocity>().with<Rock>().iter(updateVelocity);
-
-    world.system<Position, Radius>().with<Rock>().iter(debugRenderRocks);
-
-    Position p {500., 500.};
-    Velocity v {0., 0.};
-    Radius r {20.0};
-    makeRock(world, p, v, r);
 }
--- src/systems/physics.h
+++ src/systems/physics.h
@@ -1,7 +1,62 @@
 #pragma once
 
+#include "../components/particle_state.h"
 #include "flecs.h"
 
 struct PhysicSystems {
     explicit PhysicSystems(flecs::world &world);
+};
+
+constexpr float GRAVITATIONAL_CONSTANT = 1000.8;
+
+class Collisions {};
+
+class RockTools {
+
+  public:
+    /**
+     * Create new circular rock, register the new entity with the world,
+     * and initialize it with given component values.
+     *
+     * @param world
+     * @param p position
+     * @param v velocity
+     * @param r radius, should be several times larger than section width in
+     * mountain.h
+     */
+    static void makeRock(const flecs::world &world, Position p, Velocity v,
+                         Radius r);
+
+    /**
+     * Updates velocity and position of a rock.
+     *
+     * @param it
+     * @param positions
+     * @param velocities
+     */
+    static void updateState(flecs::iter it, Position *positions,
+                            Velocity *velocities);
+
+  private:
+    /**
+     * Updates velocity of a rock.
+     * //TODO so far, only force considered is gravity
+     *
+     * @param it
+     * @param positions
+     * @param velocities
+     */
+    static void updateVelocity(flecs::iter it, Position *positions,
+                               Velocity *velocities);
+
+    /**
+     * Updates position of a rock: pos_new = pos_old + v*dt.
+     * Assumes that velocities have been updated beforehand.
+     *
+     * @param it
+     * @param positions
+     * @param velocities
+     */
+    static void updatePosition(flecs::iter it, Position *positions,
+                               Velocity *velocities);
 };
