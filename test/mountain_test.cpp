7ac998b9df5a0f5d693bb612f928fc2ae3f5b274
Johannes Riemenschneider <johannes.riemenschneider@tum.de>
2023-09-21 14:40:27 +0200

    fixed error chunk generation, some mountain tests

--- test/mountain_test.cpp
+++ test/mountain_test.cpp
@@ -3,24 +3,47 @@
 #include <iostream>
 #include "src/components/mountain.h"
 
-void checkSectionIndices(Mountain& mountain ,IndexInterval interval, float min_x, float max_x){
+void checkSectionIndices(Mountain& mountain, float min_x, float max_x){
+    IndexInterval interval = mountain.getRelevantMountainSection(min_x, max_x);
+    //std::cout << "region: " << mountain.getVertice(interval.start_index).x << " " << mountain.getVertice(interval.end_index).x << " for rock: " << min_x << " " << max_x << std::endl;
     ASSERT_GE(min_x, mountain.getVertice(interval.start_index).x);
     ASSERT_GE(min_x, mountain.getVertice(interval.start_index - 1).x);
     ASSERT_GE(mountain.getVertice(interval.end_index).x, max_x);
     ASSERT_GE(mountain.getVertice(interval.end_index - 1).x, max_x);
     ASSERT_GE(max_x, mountain.getVertice(interval.end_index - 2).x);
+
+    for(std::size_t i = interval.start_index + 1; i < interval.end_index - 1; i++){
+        ASSERT_GE(mountain.getVertice(i).x, min_x);
+        ASSERT_GE(max_x, mountain.getVertice(i).x);
+    }
 }
 
-//Test Mountain Initialization and proper functionality of getMountainSection even in the edgecases
+//Test Mountain Initialization and basic relevantMountainSection functionality
 TEST(InitializationAndMountainSections, BasicAssertions) {
     Mountain mountain{};
-    float min_x = 0.5;
-    float max_x = 1.0;
-
-    checkSectionIndices(mountain, mountain.getRelevantMountainSection(min_x, max_x), min_x, max_x);
-
+    //some weird errors might occur if there are less than 56 vertices..
+    checkSectionIndices(mountain, Mountain::SECTION_WIDTH, Mountain::SECTION_WIDTH*2);
+    checkSectionIndices(mountain, Mountain::SECTION_WIDTH*16.2, Mountain::SECTION_WIDTH*24.2);
+    checkSectionIndices(mountain, Mountain::SECTION_WIDTH*2.2, Mountain::SECTION_WIDTH*4.0);
+    checkSectionIndices(mountain, Mountain::SECTION_WIDTH*45.0, Mountain::SECTION_WIDTH*54.4);
     //std::cout << firstSection.start_index << " " << firstSection.end_index << std::endl;
 }
 
+//Test whether loading new chunks breaks the mountain sections
+TEST(ChunkLoads, BasicAssertions) {
+    Mountain mountain{};
+    checkSectionIndices(mountain, Mountain::MOUNTAIN_WIDTH - Mountain::SECTION_WIDTH*10.1, Mountain::MOUNTAIN_WIDTH- Mountain::SECTION_WIDTH*2.1);
+    checkSectionIndices(mountain, Mountain::MOUNTAIN_WIDTH - Mountain::SECTION_WIDTH*8.0, Mountain::MOUNTAIN_WIDTH- Mountain::SECTION_WIDTH*2.4);
+    mountain.generateNewChunk();
+    checkSectionIndices(mountain, Mountain::MOUNTAIN_WIDTH - Mountain::SECTION_WIDTH*2.1, Mountain::MOUNTAIN_WIDTH + Mountain::SECTION_WIDTH*0.0);
+    checkSectionIndices(mountain, Mountain::MOUNTAIN_WIDTH - Mountain::SECTION_WIDTH*2.1, Mountain::MOUNTAIN_WIDTH + Mountain::SECTION_WIDTH*3.1);
+    mountain.generateNewChunk();
+    checkSectionIndices(mountain, Mountain::CHUNK_WIDTH*2 + Mountain::SECTION_WIDTH*1.1, Mountain::CHUNK_WIDTH*2 + Mountain::SECTION_WIDTH*10.2);
+    checkSectionIndices(mountain, Mountain::MOUNTAIN_WIDTH - Mountain::SECTION_WIDTH*2.1, Mountain::MOUNTAIN_WIDTH + Mountain::SECTION_WIDTH*3.1);
+    mountain.generateNewChunk();
+    checkSectionIndices(mountain, Mountain::CHUNK_WIDTH*3 + Mountain::SECTION_WIDTH*3.4, Mountain::CHUNK_WIDTH*3 + Mountain::SECTION_WIDTH*3.9);
+    checkSectionIndices(mountain, Mountain::MOUNTAIN_WIDTH + Mountain::CHUNK_WIDTH*2 + Mountain::SECTION_WIDTH*2.8,
+                        Mountain::MOUNTAIN_WIDTH + Mountain::CHUNK_WIDTH*2 + Mountain::SECTION_WIDTH*5.4);
 
-
+    //std::cout << firstSection.start_index << " " << firstSection.end_index << std::endl;
+}
