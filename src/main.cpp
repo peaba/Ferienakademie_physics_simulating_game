3e4ad8bdc2bf91208f19af3231f5216f8ae74b14
Dominik Sauerer <dominik.sauerer@gmail.com>
2023-09-22 16:48:28 +0200

    Integrate Kinect

--- src/main.cpp
+++ src/main.cpp
@@ -1,153 +1,66 @@
-/*******************************************************************************
-*                                                                              *
-*   PrimeSense NITE 1.3 - Single Control Sample                                *
-*   Copyright (C) 2010 PrimeSense Ltd.                                         *
-*                                                                              *
-*******************************************************************************/
+#include "components/mountain.h"
+#include "components/particle_state.h"
+#include "raylib.h"
+#include "systems/game_logic.h"
+#include "systems/input_systems.h"
+#include "systems/physics.h"
+#include "systems/render_systems.h"
+#include <flecs.h>
+#include <iostream>
 
-//-----------------------------------------------------------------------------
-// Headers
-//-----------------------------------------------------------------------------
-// General headers
+#ifdef kinect
 #include <cstdio>
-// OpenNI headers
 #include <XnOpenNI.h>
-// NITE headers
-#include <XnVSessionManager.h>
-#include "XnVMultiProcessFlowClient.h"
 #include <XnVWaveDetector.h>
 
 // xml to initialize OpenNI
 #define SAMPLE_XML_FILE "../../../Data/Sample-Tracking.xml"
 #define SAMPLE_XML_FILE_LOCAL "Sample-Tracking.xml"
 
-//-----------------------------------------------------------------------------
-// Callbacks
-//-----------------------------------------------------------------------------
-
-// Callback for when the focus is in progress
-void XN_CALLBACK_TYPE SessionProgress(const XnChar* strFocus, const XnPoint3D& ptFocusPoint, XnFloat fProgress, void* UserCxt)
-{
-    printf("Session progress (%6.2f,%6.2f,%6.2f) - %6.2f [%s]\n", ptFocusPoint.X, ptFocusPoint.Y, ptFocusPoint.Z, fProgress,  strFocus);
-}
-// callback for session start
-void XN_CALLBACK_TYPE SessionStart(const XnPoint3D& ptFocusPoint, void* UserCxt)
-{
-    printf("Session started. Please wave (%6.2f,%6.2f,%6.2f)...\n", ptFocusPoint.X, ptFocusPoint.Y, ptFocusPoint.Z);
-}
-// Callback for session end
-void XN_CALLBACK_TYPE SessionEnd(void* UserCxt)
-{
-    printf("Session ended. Please perform focus gesture to start session\n");
-}
-// Callback for wave detection
-void XN_CALLBACK_TYPE OnWaveCB(void* cxt)
-{
-    printf("Wave!\n");
-}
 // callback for a new position of any hand
 void XN_CALLBACK_TYPE OnPointUpdate(const XnVHandPointContext* pContext, void* cxt)
 {
     printf("%d: (%f,%f,%f) [%f]\n", pContext->nID, pContext->ptPosition.X, pContext->ptPosition.Y, pContext->ptPosition.Z, pContext->fTime);
 }
 
-//-----------------------------------------------------------------------------
-// Main
-//-----------------------------------------------------------------------------
+#endif
 
-XnBool fileExists(const char *fn)
-{
-    XnBool exists;
-    xnOSDoesFileExist(fn, &exists);
-    return exists;
+void mainLoop(flecs::world &world);
+
+int main() {
+    std::cout << "surviving sarntal" << std::endl;
+
+    flecs::world world;
+
+    #ifdef kinect
+    std::cout << "Kinect is active" << std::endl;
+    #endif
+
+    graphics::init_render_system(world);
+
+    world.import <PhysicSystems>();
+    world.import <InputSystems>();
+
+    initGameLogic(world);
+
+    world.set<Mountain>({});
+
+    world.set<AppInfo>({});
+
+    mainLoop(world);
+
+    graphics::destroy();
 }
 
+void mainLoop(flecs::world &world) {
+    world.progress(0);
 
-// this sample can run either as a regular sample, or as a client for multi-process (remote mode)
-int main(int argc, char** argv)
-{
-    xn::Context context;
-    xn::ScriptNode scriptNode;
-    XnVSessionGenerator* pSessionGenerator;
-    XnBool bRemoting = FALSE;
+    SetTargetFPS(60);
 
-    if (argc > 1)
-    {
-        // remote mode
-        context.Init();
-        printf("Running in 'Remoting' mode (Section name: %s)\n", argv[1]);
-        bRemoting = TRUE;
+    auto app_info = world.get<AppInfo>();
 
-        // Create multi-process client
-        pSessionGenerator = new XnVMultiProcessFlowClient(argv[1]);
-
-        XnStatus rc = ((XnVMultiProcessFlowClient*)pSessionGenerator)->Initialize();
-        if (rc != XN_STATUS_OK)
-        {
-            printf("Initialize failed: %s\n", xnGetStatusString(rc));
-            delete pSessionGenerator;
-            return 1;
-        }
+    while (app_info->isRunning) {
+        float dt = GetFrameTime();
+        world.progress(dt);
     }
-    else
-    {
-        // Local mode
-        // Create context
-        const char *fn = NULL;
-        if      (fileExists(SAMPLE_XML_FILE)) fn = SAMPLE_XML_FILE;
-        else if (fileExists(SAMPLE_XML_FILE_LOCAL)) fn = SAMPLE_XML_FILE_LOCAL;
-        else {
-            printf("Could not find '%s' nor '%s'. Aborting.\n" , SAMPLE_XML_FILE, SAMPLE_XML_FILE_LOCAL);
-            return XN_STATUS_ERROR;
-        }
-        XnStatus rc = context.InitFromXmlFile(fn, scriptNode);
-        if (rc != XN_STATUS_OK)
-        {
-            printf("Couldn't initialize: %s\n", xnGetStatusString(rc));
-            return 1;
-        }
-
-        // Create the Session Manager
-        pSessionGenerator = new XnVSessionManager();
-        rc = ((XnVSessionManager*)pSessionGenerator)->Initialize(&context, "Click", "RaiseHand");
-        if (rc != XN_STATUS_OK)
-        {
-            printf("Session Manager couldn't initialize: %s\n", xnGetStatusString(rc));
-            delete pSessionGenerator;
-            return 1;
-        }
-
-        // Initialization done. Start generating
-        context.StartGeneratingAll();
-    }
-
-    // Register session callbacks
-    pSessionGenerator->RegisterSession(NULL, &SessionStart, &SessionEnd, &SessionProgress);
-
-    // init & register wave control
-    XnVWaveDetector wc;
-    wc.RegisterWave(NULL, OnWaveCB);
-    wc.RegisterPointUpdate(NULL, OnPointUpdate);
-    pSessionGenerator->AddListener(&wc);
-
-    printf("Please perform focus gesture to start session\n");
-    printf("Hit any key to exit\n");
-
-    // Main loop
-    while (!xnOSWasKeyboardHit())
-    {
-        if (bRemoting)
-        {
-            ((XnVMultiProcessFlowClient*)pSessionGenerator)->ReadState();
-        }
-        else
-        {
-            context.WaitAnyUpdateAll();
-            ((XnVSessionManager*)pSessionGenerator)->Update(&context);
-        }
-    }
-
-    delete pSessionGenerator;
-
-    return 0;
 }
