f9385d2781f8f92dbebf7308b767ceb758d8c809
Axel Schneewind <axel.schneewind@protonmail.com>
2023-09-22 09:23:51 +0200

    add PlayerMovement component

--- src/components/player.h
+++ src/components/player.h
@@ -2,6 +2,15 @@
 
 struct Player {};
 
+
+struct PlayerMovement {
+    enum MovementState { MOVING, JUMPING, IN_AIR, LANDING, IDLE };
+
+    MovementState current_state;
+
+};
+
+
 struct KillBar {
     float x;
 };
