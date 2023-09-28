eb78f3f8318e50aa0ce4f614cee31ef7ddebfbac
Kilian Northoff <kilian@northoff-online.de>
2023-09-28 17:20:37 +0200

    fix

--- src/systems/game_logic.cpp
+++ src/systems/game_logic.cpp
@@ -23,7 +23,7 @@ void checkPlayerAlive(flecs::iter iter, Position *position, KillBar *killBar) {
     if (position[0].x < killBar->x) {
         std::cout << "Player Dead" << std::endl;
         iter.world().get_mut<AppInfo>()->playerAlive = false;
-        iter.entity(0).destruct();
+        // diter.entity(0).destruct();
     }
 }
 
